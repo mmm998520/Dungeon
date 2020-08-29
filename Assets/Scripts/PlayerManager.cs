cf98f9db99a927aba28efcfdc87986c3648dae57
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-08-29 09:48:18 +0800

    將在update的移動改成void move

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -15,18 +15,23 @@ namespace com.BoardGameDungeon
                 Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
                 touchPos.z = 0;
                 float targetDis = Vector3.Distance(touchPos, new Vector3(transform.position.x, transform.position.y, 0));
-                float moveDis = Time.deltaTime * moveSpeed;
                 if (targetDis < 0.5f)
                 {
-                    if (targetDis > moveDis)
-                    {
-                        transform.position += Vector3.Normalize(touchPos - new Vector3(transform.position.x, transform.position.y, 0)) * moveDis;
-                    }
-                    else
-                    {
-                        transform.position = new Vector3(touchPos.x, touchPos.y, transform.position.z);
-                    }
+                    move(touchPos, targetDis);
                 }
+            }
+        }
+
+        void move(Vector3 touchPos, float targetDis)
+        {
+            float moveDis = Time.deltaTime * moveSpeed;
+            if (targetDis > moveDis)
+            {
+                transform.position += Vector3.Normalize(touchPos - new Vector3(transform.position.x, transform.position.y, 0)) * moveDis;
+            }
+            else
+            {
+                transform.position = new Vector3(touchPos.x, touchPos.y, transform.position.z);
             }
         }
     }
