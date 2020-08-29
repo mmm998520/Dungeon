26943bc8e28e439ed39f547936c84f566ef5a86b
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-08-29 09:33:59 +0800

    Gitignore設定完成，unity新增namespace

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+namespace com.BoardGameDungeon
 {
-    float moveSpeed = 3;
-
-    void Update()
+    public class PlayerManager : MonoBehaviour
     {
-        for(int i = 0; i < Input.touchCount; i++)
+        float moveSpeed = 3;
+
+        void Update()
         {
-            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
-            touchPos.z = 0;
-            float targetDis = Vector3.Distance(touchPos, new Vector3(transform.position.x, transform.position.y, 0));
-            float moveDis = Time.deltaTime * moveSpeed;
-            if (targetDis < 0.5f)
+            for (int i = 0; i < Input.touchCount; i++)
             {
-                if(targetDis > moveDis)
+                Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+                touchPos.z = 0;
+                float targetDis = Vector3.Distance(touchPos, new Vector3(transform.position.x, transform.position.y, 0));
+                float moveDis = Time.deltaTime * moveSpeed;
+                if (targetDis < 0.5f)
                 {
-                    transform.position += Vector3.Normalize(touchPos - new Vector3(transform.position.x, transform.position.y, 0)) * moveDis;
-                }
-                else
-                {
-                    transform.position = new Vector3(touchPos.x, touchPos.y, transform.position.z);
+                    if (targetDis > moveDis)
+                    {
+                        transform.position += Vector3.Normalize(touchPos - new Vector3(transform.position.x, transform.position.y, 0)) * moveDis;
+                    }
+                    else
+                    {
+                        transform.position = new Vector3(touchPos.x, touchPos.y, transform.position.z);
+                    }
                 }
             }
         }
