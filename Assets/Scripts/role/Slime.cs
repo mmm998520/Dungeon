32086aa11c87584f07c9a536cd541ef8d8c0a4da
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-09-03 15:13:01 +0800

    史萊姆優先區域完成，閃人還有點問題

--- Assets/Scripts/role/Slime.cs
+++ Assets/Scripts/role/Slime.cs
@@ -23,44 +23,24 @@ namespace com.BoardGameDungeon
             monsterType = MonsterType.Slime;
 
             #region//決定範圍
-            side = new Transform[MazeGen.row*MazeGen.col - 15];
-            mid = new Transform[15];
-            for (int i = 0; i < mid.Length; i++)
+            side = new Transform[68];
+            mid = new Transform[12];
+            int sideNum = 0, midNum = 0;
+            for(int i = 0; i < MazeGen.row; i++)
             {
-                float min = float.MaxValue;
-                Transform nearestFloor = GameManager.Floors.GetChild(0);
-                foreach (Transform child in GameManager.Floors)
+                for(int j = 0; j < MazeGen.col; j++)
                 {
-                    if (min > Vector3.Distance(transform.position, child.position))
+                    if (i < 2 || j < 2 || i > MazeGen.row - 3 || j > MazeGen.col - 3)
                     {
-                        if (i < mid.Length)
-                        {
-                            if (!mid.Contains(child))
-                            {
-                                min = Vector3.Distance(transform.position, child.position);
-                                nearestFloor = child;
-                            }
-                        }
+                        side[sideNum++] = GameManager.Floors.GetChild(i * MazeGen.col + j);
                     }
-                }
-                mid[i] = nearestFloor;
-            }
-            for (int i = 0; i < side.Length; i++)
-            {
-                foreach (Transform child in GameManager.Floors)
-                {
-                    if (!mid.Contains(child) && !side.Contains(child))
+                    if(i > 3 && j > 2 && i < MazeGen.row - 4 && j < MazeGen.col - 4)
                     {
-                        side[i] = child;
-                        break;
+                        mid[midNum++] = GameManager.Floors.GetChild(i * MazeGen.col + j);
                     }
                 }
             }
             #endregion
-            if(side[side.Length - 1]== null)
-            {
-                Debug.LogError("side沒裝滿");
-            }
             Invoke("reNavigate", 0.01f);
         }
 
@@ -211,6 +191,33 @@ namespace com.BoardGameDungeon
             //生成攻擊在自己腳下
             GameObject attack = Instantiate(MonsterAttack[(int)monsterType], transform.position, Quaternion.identity);
             attack.GetComponent<AttackManager>().setValue(ATK[(int)monsterType, 0], duration[(int)monsterType], continuous[(int)monsterType], false);
+        }
+
+        protected override float priority(float dis, int nextRow, int nextCol)
+        {
+            //距離玩家還很遠就盡量不要進中心
+            if (straightTarget.Distance > 3)
+            {
+                if (nextRow < MazeGen.row - 3 && nextRow >= 3 && nextCol < MazeGen.col - 3 && nextCol >= 3)
+                {
+                    dis += 10;
+                }
+            }
+            //距離玩家很近就避開會面向玩家的道路
+            else
+            {
+                float minDis = 99999;
+                Vector3 nextPos = new Vector3(nextRow * 2 + 1, nextCol * 2 + 1);
+                foreach (Transform player in GameManager.Players)
+                {
+                    if(minDis > Vector3.Distance(nextPos, player.position))
+                    {
+                        minDis = Vector3.Distance(nextPos, player.position);
+                    }
+                }
+                dis += 5 / minDis;
+            }
+            return dis;
         }
     }
 }
