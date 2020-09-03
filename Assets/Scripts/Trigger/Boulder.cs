ec12e751f8ad92ad3051a4dc47edb4bee831ad6d
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-09-03 14:25:57 +0800

    修復怪物會觸發滾石BUG

--- Assets/Scripts/Trigger/Boulder.cs
+++ Assets/Scripts/Trigger/Boulder.cs
@@ -20,48 +20,51 @@ namespace com.BoardGameDungeon
 
         void OnTriggerEnter2D(Collider2D collision)
         {
-            int row, col;
+            if (collision.GetComponent<PlayerManager>())
+            {
+                int row, col;
 
-            for (row = 0; row < MazeGen.row; row++)
-            {
-                if (Mathf.Abs(transform.position.x - (row * 2 + 1)) <= 1)
+                for (row = 0; row < MazeGen.row; row++)
                 {
-                    break;
+                    if (Mathf.Abs(transform.position.x - (row * 2 + 1)) <= 1)
+                    {
+                        break;
+                    }
                 }
-            }
-            for (col = 0; col < MazeGen.Creat_col; col++)
-            {
-                if (Mathf.Abs(transform.position.y - (col * 2 + 1)) <= 1)
+                for (col = 0; col < MazeGen.Creat_col; col++)
                 {
-                    break;
+                    if (Mathf.Abs(transform.position.y - (col * 2 + 1)) <= 1)
+                    {
+                        break;
+                    }
                 }
-            }
-            int r = Random.Range(0, 4);
-            GameObject stone;
-            Vector3 dir;
-            if (r >= 3)
-            {
-                stone = Instantiate(this.stone, new Vector3(row * 2 + 1, (MazeGen.col - 1) * 2 + 1), Quaternion.identity);
-                dir = Vector3.down;
-            }
-            else if (r >= 2)
-            {
-                stone = Instantiate(this.stone, new Vector3(row * 2 + 1, 0 * 2 + 1), Quaternion.identity);
-                dir = Vector3.up;
-            }
-            else if (r >= 1)
-            {
-                stone = Instantiate(this.stone, new Vector3((MazeGen.row - 1) * 2 + 1, col * 2 + 1), Quaternion.identity);
-                dir = Vector3.left;
-            }
-            else
-            {
-                stone = Instantiate(this.stone, new Vector3(0 * 2 + 1, col * 2 + 1), Quaternion.identity);
-                dir = Vector3.right;
-            }
-            stone.GetComponent<Stone>().dir = dir;
+                int r = Random.Range(0, 4);
+                GameObject stone;
+                Vector3 dir;
+                if (r >= 3)
+                {
+                    stone = Instantiate(this.stone, new Vector3(row * 2 + 1, (MazeGen.col - 1) * 2 + 1), Quaternion.identity);
+                    dir = Vector3.down;
+                }
+                else if (r >= 2)
+                {
+                    stone = Instantiate(this.stone, new Vector3(row * 2 + 1, 0 * 2 + 1), Quaternion.identity);
+                    dir = Vector3.up;
+                }
+                else if (r >= 1)
+                {
+                    stone = Instantiate(this.stone, new Vector3((MazeGen.row - 1) * 2 + 1, col * 2 + 1), Quaternion.identity);
+                    dir = Vector3.left;
+                }
+                else
+                {
+                    stone = Instantiate(this.stone, new Vector3(0 * 2 + 1, col * 2 + 1), Quaternion.identity);
+                    dir = Vector3.right;
+                }
+                stone.GetComponent<Stone>().dir = dir;
 
-            Destroy(gameObject);
+                Destroy(gameObject);
+            }
         }
     }
 }
