1f95d7fdda44f6cfc8cb9998690799adf1dd9ca3
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-09-05 23:05:56 +0800

    石頭改2倍大、重寫石頭邏輯、出口改顏色

--- Assets/Scripts/Trigger/Stone.cs
+++ Assets/Scripts/Trigger/Stone.cs
@@ -9,21 +9,39 @@ namespace com.BoardGameDungeon
         public Vector3 dir;
         float speed = 3;
         float ATK = 5;
+        
         public PlayerManager user;
 
         void Update()
         {
             transform.Translate(dir * Time.deltaTime * speed);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, transform.lossyScale.x,1<<8);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, transform.lossyScale.x / 2, 1<<8);
+            Vector3 tempDir = Quaternion.Euler(0, 0, 90) * dir.normalized;
+            RaycastHit2D hit1 = Physics2D.Raycast(transform.position + tempDir, dir, transform.lossyScale.x / 10, 1<<8);
+            RaycastHit2D hit2 = Physics2D.Raycast(transform.position - tempDir, dir, transform.lossyScale.x / 10, 1<<8);
             if (hit)
             {
-                if (hit.collider.tag == "wall")
+                if (hit.collider.tag == "side")
+                {
+                    Destroy(gameObject);
+                }
+                else if (hit.collider.tag == "wall")
                 {
                     Destroy(hit.collider.gameObject);
                 }
-                else if (hit.collider.tag == "side")
+            }
+            if (hit1)
+            {
+                if (hit1.collider.tag == "wall")
                 {
-                    Destroy(gameObject);
+                    Destroy(hit1.collider.gameObject);
+                }
+            }
+            if (hit2)
+            {
+                if (hit2.collider.tag == "wall")
+                {
+                    Destroy(hit2.collider.gameObject);
                 }
             }
         }
@@ -33,14 +51,6 @@ namespace com.BoardGameDungeon
             if (collider.GetComponent<ValueSet>())
             {
                 collider.GetComponent<ValueSet>().Hurt += ATK;
-            }
-            else if(collider.tag == "wall")
-            {
-                Destroy(collider.gameObject);
-            }
-            else if(collider.tag == "side")
-            {
-                Destroy(gameObject);
             }
             MonsterManager.addHurtMe(collider, user);
         }
