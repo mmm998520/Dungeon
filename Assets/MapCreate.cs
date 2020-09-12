1aa170496e763890b48ec8467c8ea4fe517ef947
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-09-12 17:01:30 +0800

    地圖生成器測試版完成

---
+++ Assets/MapCreate.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MapCreate : MonoBehaviour
+{
+    public static int row = 70;
+    public static int col = 70;
+    private bool[,] mapArray;
+    public GameObject cube1, cube2;
+    GameObject cubes;
+    int times;
+    int a;
+
+
+    void Start()
+    {
+        cubes = new GameObject();
+        mapArray = InitMapArray();
+        CreateMap(mapArray);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Button1();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Button2();
+        }
+    }
+
+    /// <summary> 隨機生成地圖資料庫 </summary>
+    bool[,] InitMapArray()
+    {
+        bool[,] array = new bool[row, col];
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                //60%機率true反之false
+                array[i, j] = Random.Range(0, 100) < 60;
+                //外壁固定為牆
+                if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
+                {
+                    array[i, j] = false;
+                }
+            }
+        }
+        return array;
+    }
+
+    /// <summary> 潤飾，讓隨機數據符合周圍地圖 </summary>
+    bool[,] SmoothMapArray(bool[,] array)
+    {
+        bool[,] newArray = new bool[row, col];
+        int count1, count2;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                count1 = CheckNeighborWalls(array, i, j, 1);
+                count2 = CheckNeighborWalls(array, i, j, 2);
+
+                if (count1 >= 5 || count2 <= 2)
+                {
+                    newArray[i, j] = false;
+                }
+                else
+                {
+                    newArray[i, j] = true;
+                }
+
+                if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
+                {
+                    newArray[i, j] = false;
+                }
+            }
+        }
+        return newArray;
+    }
+
+    /// <summary> 查看點周圍的牆壁數，t圍距離圈(t=1九宮格，t=2二十五宮格) </summary>
+    int CheckNeighborWalls(bool[,] array, int i, int j, int t)
+    {
+        int count = 0;
+        for (int i2 = i - t; i2 < i + t + 1; i2++)
+        {
+            for (int j2 = j - t; j2 < j + t + 1; j2++)
+            {
+                if (i2 > 0 && i2 < row && j2 >= 0 && j2 < col)
+                {
+                    if (!array[i2, j2])
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        if (!array[i, j])
+            count--;
+        return count;
+    }
+
+
+    void CreateMap(bool[,] array)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (!array[i, j])
+                {
+                    GameObject go = Instantiate(cube1, new Vector3(i, 1, j), Quaternion.identity) as GameObject;
+                    go.transform.SetParent(cubes.transform);
+                }
+                else
+                {
+                    GameObject go = Instantiate(cube2, new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    go.transform.SetParent(cubes.transform);
+                }
+            }
+        }
+    }
+
+
+    public void Button1()
+    {
+        times = 0;
+        Destroy(cubes);
+        cubes = new GameObject();
+        mapArray = InitMapArray();
+        CreateMap(mapArray);
+    }
+
+
+    public void Button2()
+    {
+        //if (times < 20)
+        {
+            times++;
+            Destroy(cubes);
+            cubes = new GameObject();
+            mapArray = SmoothMapArray(mapArray);
+            CreateMap(mapArray);
+        }
+    }
+}
---
+++ Assets/MapRange.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRange : MonoBehaviour
+{
+    int times;
+    private int[,] mapArray;
+    GameObject cubes;
+    public GameObject[] cube = new GameObject[4];
+
+    public enum roomStat
+    {
+        green = 0,
+        black = 1,
+        blue = 2,
+        orange = 3
+    }
+    void Start()
+    {
+        cubes = new GameObject();
+        mapArray = InitMapArray();
+        CreateMap(mapArray);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Button1();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Button2();
+        }
+    }
+
+    int[,] InitMapArray()
+    {
+        int[,] array = new int[MapCreate.row, MapCreate.col];
+        for (int i = 0; i < MapCreate.row; i++)
+        {
+            for (int j = 0; j < MapCreate.col; j++)
+            {
+                if (i >= MapCreate.row/7*3 && i <= MapCreate.row / 7 * 4 && j >= MapCreate.col / 7 * 3 && j <= MapCreate.row / 7 * 4)
+                {
+                    array[i, j] = (int)roomStat.green;
+                }
+                else if(i >= MapCreate.row / 7 * 2 && i <= MapCreate.row / 7 * 5 && j >= MapCreate.col / 7 * 2 && j <= MapCreate.row / 7 * 5)
+                {
+                    array[i, j] = (int)roomStat.black;
+                }
+                else
+                {
+                    int r = Random.Range(0, 11);
+                    if (r >= 10)
+                    {
+                        array[i, j] = (int)roomStat.black;
+                    }
+                    else if(r >= 5)
+                    {
+                        array[i, j] = (int)roomStat.blue;
+                    }
+                    else
+                    {
+                        array[i, j] = (int)roomStat.orange;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    int[,] SmoothMapArray(int[,] array)
+    {
+        int[,] newArray = new int[MapCreate.row, MapCreate.col];
+        int[] count;
+        for (int i = 0; i < MapCreate.row; i++)
+        {
+            for (int j = 0; j < MapCreate.col; j++)
+            {
+                count = CheckNeighborWalls(array, i, j, 1);
+                if (count[0] > 4)
+                {
+                    newArray[i, j] = (int)roomStat.green;
+                }
+                else if(count[0] == 4)
+                {
+                    int r = Random.Range(0, 2);
+                    newArray[i, j] = (int)roomStat.green + r;
+                }
+                else if(count[1] >= 4)
+                {
+                    newArray[i, j] = (int)roomStat.black;
+                }
+                else if(count[2] > 4)
+                {
+                    newArray[i, j] = (int)roomStat.blue;
+                }
+                else if (count[3] > 4)
+                {
+                    newArray[i, j] = (int)roomStat.orange;
+                }
+                else
+                {
+                    int r = Random.Range(0, 2);
+                    newArray[i, j] = (int)roomStat.blue + r;
+                }
+            }
+        }
+        return newArray;
+    }
+
+    int[] CheckNeighborWalls(int[,] array, int i, int j, int t)
+    {
+        int[] count = new int[4] { 0, 0, 0, 0 };
+        for (int i2 = i - t; i2 < i + t + 1; i2++)
+        {
+            for (int j2 = j - t; j2 < j + t + 1; j2++)
+            {
+                if (i2 >= 0 && i2 < MapCreate.row && j2 >= 0 && j2 < MapCreate.col)
+                {
+                    count[array[i2, j2]]++;
+                }
+            }
+        }
+        count[array[i, j]]--;
+        return count;
+    }
+
+    void CreateMap(int[,] array)
+    {
+        for (int i = 0; i < MapCreate.row; i++)
+        {
+            for (int j = 0; j < MapCreate.col; j++)
+            {
+                GameObject go = Instantiate(cube[array[i, j]], new Vector3(i, 1, j), Quaternion.identity);
+                go.transform.SetParent(cubes.transform);
+            }
+        }
+    }
+
+    public void Button1()
+    {
+        times = 0;
+        Destroy(cubes);
+        cubes = new GameObject();
+        mapArray = InitMapArray();
+        CreateMap(mapArray);
+    }
+
+
+    public void Button2()
+    {
+        //if (times < 20)
+        {
+            times++;
+            Destroy(cubes);
+            cubes = new GameObject();
+            mapArray = SmoothMapArray(mapArray);
+            CreateMap(mapArray);
+        }
+    }
+}
