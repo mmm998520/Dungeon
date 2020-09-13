390bd9c9a6b89b319d826bb3cc98127e34dca7b6
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-09-13 20:24:19 +0800

    確保單位區域不會過小

--- Assets/Script/MapCreater.cs
+++ Assets/Script/MapCreater.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapRange : MonoBehaviour
+public class MapCreater : MonoBehaviour
 {
     int times;
     public static int[,] mapArray;
     public static int[,] TempArray;
     GameObject cubes;
     public GameObject[] cube = new GameObject[4];
+    public const int row = 70, col = 70;
 
     public enum roomStat
     {
@@ -18,7 +19,13 @@ public class MapRange : MonoBehaviour
         blue = 3,
         orange = 4
     }
+
     void Start()
+    {
+        StartCoroutine("s");
+    }
+
+    IEnumerator s()
     {
         int t = 0;
         do
@@ -27,49 +34,42 @@ public class MapRange : MonoBehaviour
             Destroy(cubes);
             cubes = new GameObject();
             Button1();
-            MapCreate.mapCreate.reMapArray();
+            WallCreater.wallCreater.reMapArray();
             mapArray = InitMapArray();
             for (int i = 0; i < 7; i++)
             {
                 times++;
                 mapArray = SmoothMapArray(mapArray);
             }
-            TempArray = CreateMap(mapArray);
-            if (t > 10)
+            TempArray = AddMapWall(mapArray);
+            createMap(TempArray);
+            yield return new WaitForSeconds(2);
+            Destroy(cubes);
+            cubes = new GameObject();
+            TempArray = checkNum(TempArray);
+            createMap(TempArray);
+            yield return new WaitForSeconds(2);
+            if (t > 30)
             {
-                print("aaaaaaaaaaaaaaaaaaaaaaaa");
                 break;
             }
         } while (!checkCanGo(TempArray) || !GreenWayNotSeparate(TempArray));
         print(t);
-        print(checkCanGo(TempArray));
-        print(GreenWayNotSeparate(TempArray));
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Button1();
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Button2();
-        }
     }
 
     int[,] InitMapArray()
     {
-        int[,] array = new int[MapCreate.row, MapCreate.col];
-        for (int i = 0; i < MapCreate.row; i++)
+        int i, j;
+        int[,] array = new int[row, col];
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < MapCreate.col; j++)
+            for (j = 0; j < col; j++)
             {
-                if (i >= MapCreate.row/7*3 && i <= MapCreate.row / 7 * 4 && j >= MapCreate.col / 7 * 3 && j <= MapCreate.row / 7 * 4)
+                if (i >= row/7*3 && i <= row / 7 * 4 && j >= col / 7 * 3 && j <= row / 7 * 4)
                 {
                     array[i, j] = (int)roomStat.green;
                 }
-                else if(i >= MapCreate.row / 7 * 2 && i <= MapCreate.row / 7 * 5 && j >= MapCreate.col / 7 * 2 && j <= MapCreate.row / 7 * 5)
+                else if(i >= row / 7 * 2 && i <= row / 7 * 5 && j >= col / 7 * 2 && j <= row / 7 * 5)
                 {
                     array[i, j] = (int)roomStat.black;
                 }
@@ -96,11 +96,12 @@ public class MapRange : MonoBehaviour
 
     int[,] SmoothMapArray(int[,] array)
     {
-        int[,] newArray = new int[MapCreate.row, MapCreate.col];
+        int i, j;
+        int[,] newArray = new int[row, col];
         int[] count;
-        for (int i = 0; i < MapCreate.row; i++)
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < MapCreate.col; j++)
+            for (j = 0; j < col; j++)
             {
                 count = CheckNeighborWalls(array, i, j, 1);
                 if (count[(int)roomStat.green] > 4)
@@ -164,14 +165,15 @@ public class MapRange : MonoBehaviour
 
     int[] CheckNeighborWalls(int[,] array, int i, int j, int t)
     {
+        int _i, _j;
         int[] count = new int[5] { 0, 0, 0, 0, 0 };
-        for (int i2 = i - t; i2 < i + t + 1; i2++)
+        for (_i = i - t; _i < i + t + 1; _i++)
         {
-            for (int j2 = j - t; j2 < j + t + 1; j2++)
+            for (_j = j - t; _j < j + t + 1; _j++)
             {
-                if (i2 >= 0 && i2 < MapCreate.row && j2 >= 0 && j2 < MapCreate.col)
+                if (_i >= 0 && _i < row && _j >= 0 && _j < col)
                 {
-                    count[array[i2, j2]]++;
+                    count[array[_i, _j]]++;
                 }
             }
         }
@@ -179,69 +181,73 @@ public class MapRange : MonoBehaviour
         return count;
     }
 
-    int[,] CreateMap(int[,] array)
+    int[,] AddMapWall(int[,] array)
     {
-        int[,] newArray = new int[MapCreate.row, MapCreate.col];
-        for (int i = 0; i < MapCreate.row; i++)
+        int i, j;
+        int[,] newArray = new int[row, col];
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < MapCreate.col; j++)
+            for (j = 0; j < col; j++)
             {
-                GameObject go;
-                if (MapCreate.mapArray[i, j])
+                if (WallCreater.mapArray[i, j])
                 {
-                    go = Instantiate(cube[array[i, j]], new Vector3(i, 1, j), Quaternion.identity);
                     newArray[i, j] = array[i, j];
                 }
                 else
                 {
-                    go = Instantiate(cube[0], new Vector3(i, 1, j), Quaternion.identity);
                     newArray[i, j] = 0;
                 }
-                go.transform.SetParent(cubes.transform);
             }
         }
         return newArray;
     }
 
+    void createMap(int[,] array)
+    {
+        int i, j;
+        GameObject go;
+        for (i = 0; i < row; i++)
+        {
+            for (j = 0; j < col; j++)
+            {
+                if (WallCreater.mapArray[i, j])
+                {
+                    go = Instantiate(cube[array[i, j]], new Vector3(i, j, 1), Quaternion.identity);
+                }
+                else
+                {
+                    go = Instantiate(cube[0], new Vector3(i, j, 1), Quaternion.identity);
+                }
+                go.transform.SetParent(cubes.transform);
+            }
+        }
+    }
+
     public void Button1()
     {
         times = 0;
-        Destroy(cubes);
-        cubes = new GameObject();
         mapArray = InitMapArray();
-    }
-
-
-    public void Button2()
-    {
-        //if (times < 20)
-        {
-            times++;
-            Destroy(cubes);
-            cubes = new GameObject();
-            mapArray = SmoothMapArray(mapArray);
-            CreateMap(mapArray);
-        }
     }
 
     bool checkCanGo(int[,] array)
     {
+        int t, j, i;
         //建立可通行路徑清單，由第一個偵測到的綠色區域作為起始點
-        bool[,] canGoArray = new bool[MapCreate.row, MapCreate.col];
+        bool[,] canGoArray = new bool[row, col];
         //必須到達點
         int[,] mustGo = new int[9, 2];
-        for (int i = 0; i < MapCreate.row; i++)
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < MapCreate.col; j++)
+            for (j = 0; j < col; j++)
             {
                 canGoArray[i, j] = false;
             }
         }
-        for(int t = 0; t < 9; t++)
+        for(t = 0; t < 9; t++)
         {
-            for (int i = MapCreate.row / 7 * (3 * (t / 3)); i < MapCreate.row / 7 * (3 * (t / 3) + 1) ; i++)
+            for (i = row / 7 * (3 * (t / 3)); i < row / 7 * (3 * (t / 3) + 1) ; i++)
             {
-                for (int j = MapCreate.row / 7 * (3 * (t % 3)); j < MapCreate.col / 7 * (3 * (t % 3) + 1); j++)
+                for (j = row / 7 * (3 * (t % 3)); j < col / 7 * (3 * (t % 3) + 1); j++)
                 {
                     if (t == 4)
                     {
@@ -272,15 +278,15 @@ public class MapRange : MonoBehaviour
             Break:;
         }
         //從最左上方的必須抵達點掃看能抵達點，若所有必需抵達點都為true則成功，反之失敗
-        for (int t = 0; t < MapCreate.row * MapCreate.col; t++)
+        for (t = 0; t < 100; t++)
         {
-            for (int i = 0; i < MapCreate.row; i++)
+            for (i = 0; i < row; i++)
             {
-                for (int j = 0; j < MapCreate.col; j++)
+                for (j = 0; j < col; j++)
                 {
                     if (canGoArray[i, j])
                     {
-                        if(i + 1 < MapCreate.row)
+                        if(i + 1 < row)
                         {
                             if (array[i + 1, j] != (int)roomStat.wall)
                             {
@@ -294,7 +300,7 @@ public class MapRange : MonoBehaviour
                                 canGoArray[i - 1, j] = true;
                             }
                         }
-                        if (j + 1 < MapCreate.col)
+                        if (j + 1 < col)
                         {
                             if (array[i, j + 1] != (int)roomStat.wall)
                             {
@@ -312,7 +318,7 @@ public class MapRange : MonoBehaviour
                 }
             }
 
-            for(int i = 0; i< mustGo.Length / 2; i++)
+            for(i = 0; i< mustGo.Length / 2; i++)
             {
                 if(!canGoArray[mustGo[i,0], mustGo[i, 1]])
                 {
@@ -329,18 +335,19 @@ public class MapRange : MonoBehaviour
 
     bool GreenWayNotSeparate(int[,] array)
     {
-        bool[,] canGoArray = new bool[MapCreate.row, MapCreate.col];
+        int t, i, j;
+        bool[,] canGoArray = new bool[row, col];
         List<int[]> mustGo = new List<int[]>();
-        for (int i = 0; i < MapCreate.row; i++)
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < MapCreate.col; j++)
+            for (j = 0; j < col; j++)
             {
                 canGoArray[i, j] = false;
             }
         }
-        for (int i = 0; i < MapCreate.row; i++)
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < MapCreate.col; j++)
+            for (j = 0; j < col; j++)
             {
                 if (array[i, j] == (int)roomStat.green)
                 {
@@ -357,15 +364,15 @@ public class MapRange : MonoBehaviour
             return false;
         }
         //從最左上方的必須抵達點掃看能抵達點，若所有必需抵達點都為true則成功，反之失敗
-        for (int t = 0; t < MapCreate.row * MapCreate.col; t++)
+        for (t = 0; t < 100; t++)
         {
-            for (int i = 0; i < MapCreate.row; i++)
+            for (i = 0; i < row; i++)
             {
-                for (int j = 0; j < MapCreate.col; j++)
+                for (j = 0; j < col; j++)
                 {
                     if (canGoArray[i, j])
                     {
-                        if (i + 1 < MapCreate.row)
+                        if (i + 1 < row)
                         {
                             if (array[i + 1, j] == (int)roomStat.green)
                             {
@@ -379,7 +386,7 @@ public class MapRange : MonoBehaviour
                                 canGoArray[i - 1, j] = true;
                             }
                         }
-                        if (j + 1 < MapCreate.col)
+                        if (j + 1 < col)
                         {
                             if (array[i, j + 1] == (int)roomStat.green)
                             {
@@ -397,7 +404,7 @@ public class MapRange : MonoBehaviour
                 }
             }
 
-            for (int i = 0; i < mustGo.Count; i++)
+            for (i = 0; i < mustGo.Count; i++)
             {
                 if (!canGoArray[mustGo[i][0], mustGo[i][1]])
                 {
@@ -410,5 +417,160 @@ public class MapRange : MonoBehaviour
             }
         }
         return false;
+    }
+
+    int[,] checkNum(int[,] array)
+    {
+        int t, i, j;
+        bool[,] Checked = new bool[row, col];
+        int[,] type = new int[row, col];
+        List<int> typeNum = new List<int>();
+        typeNum.Add(0);
+        //先確認各區域分別有幾個格子
+        for (i = 0; i < row; i++)
+        {
+            for (j = 0; j < col; j++)
+            {
+                Checked[i, j] = false;
+                type[i, j] = 0;
+            }
+        }
+        int times = 0;
+        do
+        {
+            if (times++ > 1000)
+            {
+                print(times);
+                break;
+            }
+            //將最前方的標記為新起始
+            for (i = 0; i < row; i++)
+            {
+                for (j = 0; j < col; j++)
+                {
+                    if(type[i, j] == 0)
+                    {
+                        type[i, j] = typeNum.Count;
+                        print(type[i, j]);
+                        typeNum.Add(1);
+                        goto FindRoad;
+                    }
+                }
+            }
+            //沒觸發goto則代表全都搜尋完畢了
+            break;
+        FindRoad:;
+            //從起始待使向外找尋可同型路徑，直到沒有路為止
+            t = 0;
+            int times2 = 0;
+            do
+            {
+                if (times2++ > 100)
+                {
+                    print(times2);
+                    break;
+                }
+                for (i = 0; i < row; i++)
+                {
+                    for (j = 0; j < col; j++)
+                    {
+                        if (!Checked[i, j] && type[i, j] != 0)
+                        {
+                            if (i + 1 < row)
+                            {
+                                if (array[i, j] == array[i + 1, j] && type[i + 1, j] == 0)
+                                {
+                                    t++;
+                                    type[i + 1, j] = type[i, j];
+                                    typeNum[type[i, j]]++;
+                                }
+                            }
+                            if (i - 1 >= 0)
+                            {
+                                if (array[i, j] == array[i - 1, j] && type[i - 1, j] == 0)
+                                {
+                                    t++;
+                                    type[i - 1, j] = type[i, j];
+                                    typeNum[type[i, j]]++;
+                                }
+                            }
+                            if (j + 1 < col)
+                            {
+                                if (array[i, j] == array[i, j + 1] && type[i, j + 1] == 0)
+                                {
+                                    t++;
+                                    type[i, j + 1] = type[i, j];
+                                    typeNum[type[i, j]]++;
+                                }
+                            }
+                            if (j - 1 >= 0)
+                            {
+                                if (array[i, j] == array[i, j - 1] && type[i, j - 1] == 0)
+                                {
+                                    t++;
+                                    type[i, j - 1] = type[i, j];
+                                    typeNum[type[i, j]]++;
+                                }
+                            }
+                            Checked[i, j] = true;
+                        }
+                    }
+                }
+            } while (t > 0);
+        } while (true);
+        /*
+        for (i = 0; i < row; i++)
+        {
+            for(j = 0; j < col; j++)
+            {
+                if(type[i,j] == 0)
+                {
+                    type[i, j] = typeNum.Count;
+                    typeNum.Add(1);
+                }
+                if (i + 1 < row)
+                {
+                    if (array[i, j] == array[i + 1, j] && type[i + 1, j] == 0)
+                    {
+                        type[i + 1, j] = type[i, j];
+                        typeNum[type[i, j]]++;
+                    }
+                }
+                if (j + 1 < col)
+                {
+                    if (array[i, j] == array[i, j + 1] && type[i, j + 1] == 0)
+                    {
+                        type[i, j + 1] = type[i, j];
+                        typeNum[type[i, j]]++;
+                    }
+                }
+            }
+        }
+        */
+        //將格子數量太少的挑出來
+        for (i = 0; i < row; i++)
+        {
+            for (j = 0; j < col; j++)
+            {
+                if (typeNum[type[i, j]] < 10)
+                {
+                    if (i - 1 >= 0 && array[i,j] != (int)roomStat.wall)
+                    {
+                        array[i, j] = array[i - 1, j];
+                    }
+                }
+            }
+        }
+        return array;
+
+        /*
+        int a = 0;
+        foreach(int k in typeNum)
+        {
+            a += k;
+        }
+        print(a);
+        print(typeNum.Count);
+        */
     }
 }
--- Assets/Script/WallCreater.cs
+++ Assets/Script/WallCreater.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 
 
-public class MapCreate : MonoBehaviour
+public class WallCreater : MonoBehaviour
 {
-    public static int row = 70;
-    public static int col = 70;
+    public int row,col;
     public static bool[,] mapArray;
-    public static MapCreate mapCreate;
+    public static WallCreater wallCreater;
     void Start()
     {
-        mapCreate = this;
+        row = MapCreater.row;
+        col = MapCreater.col;
+        wallCreater = this;
         reMapArray();
     }
 
@@ -23,25 +24,14 @@ public class MapCreate : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Button1();
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Button2();
-        }
-    }
-
     /// <summary> 隨機生成地圖資料庫 </summary>
     bool[,] InitMapArray()
     {
+        int i, j;
         bool[,] array = new bool[row, col];
-        for (int i = 0; i < row; i++)
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < col; j++)
+            for (j = 0; j < col; j++)
             {
                 //60%機率true反之false
                 array[i, j] = Random.Range(0, 100) < 60;
@@ -58,11 +48,12 @@ public class MapCreate : MonoBehaviour
     /// <summary> 潤飾，讓隨機數據符合周圍地圖 </summary>
     bool[,] SmoothMapArray(bool[,] array)
     {
+        int i, j;
         bool[,] newArray = new bool[row, col];
         int count1, count2;
-        for (int i = 0; i < row; i++)
+        for (i = 0; i < row; i++)
         {
-            for (int j = 0; j < col; j++)
+            for (j = 0; j < col; j++)
             {
                 count1 = CheckNeighborWalls(array, i, j, 1);
                 count2 = CheckNeighborWalls(array, i, j, 2);
@@ -88,14 +79,15 @@ public class MapCreate : MonoBehaviour
     /// <summary> 查看點周圍的牆壁數，t圍距離圈(t=1九宮格，t=2二十五宮格) </summary>
     int CheckNeighborWalls(bool[,] array, int i, int j, int t)
     {
+        int _i, _j;
         int count = 0;
-        for (int i2 = i - t; i2 < i + t + 1; i2++)
+        for (_i = i - t; _i < i + t + 1; _i++)
         {
-            for (int j2 = j - t; j2 < j + t + 1; j2++)
+            for (_j = j - t; _j < j + t + 1; _j++)
             {
-                if (i2 > 0 && i2 < row && j2 >= 0 && j2 < col)
+                if (_i > 0 && _i < row && _j >= 0 && _j < col)
                 {
-                    if (!array[i2, j2])
+                    if (!array[_i, _j])
                     {
                         count++;
                     }
@@ -105,19 +97,5 @@ public class MapCreate : MonoBehaviour
         if (!array[i, j])
             count--;
         return count;
-    }
-
-    public void Button1()
-    {
-        mapArray = InitMapArray();
-    }
-
-
-    public void Button2()
-    {
-        //if (times < 20)
-        {
-            mapArray = SmoothMapArray(mapArray);
-        }
     }
 }
