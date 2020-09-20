17b40f4b87a13149e4b347f7c5c127dfaafb5b56
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-09-20 15:26:51 +0800

    怪物攻擊邏輯完成

--- Assets/Script/MonsterManager.cs
+++ Assets/Script/MonsterManager.cs
@@ -6,11 +6,23 @@ namespace com.DungeonPad
 {
     public class MonsterManager : DirectionChanger
     {
-        public float MaxHP,HP,ATK;
+        public float MaxHP, HP, ATK, CD, CDTimer, Preparation, PreparationTimer, hand;
+        public GameObject attack;
         /// <summary> 守備區域 </summary>
         protected HashSet<int> guardPos = new HashSet<int>();
         /// <summary> 追擊區域 </summary>
         protected HashSet<int> pursuePos = new HashSet<int>();
+        public int[] guardPoint;
+        public int nextGrardNum;
+        enum Stat
+        {
+            guard,
+            pursue,
+            back
+        }
+        Stat stat = Stat.guard;
+
+
         /// <summary> 將怪物誕生區域設為守備區域並給予追擊範圍 </summary>
         protected void setCanGoPos()
         {
@@ -56,6 +68,8 @@ namespace com.DungeonPad
             }
         }
 
+
+        /// <summary> 獲取最近玩家 </summary>
         protected Transform MinDisPlayer()
         {
             int i;
@@ -73,10 +87,80 @@ namespace com.DungeonPad
             return minDisPlayer;
         }
 
+
+        /// <summary> 轉向並向下一個目標點前進 </summary>
         protected void moveToTarget()
         {
             changeDirection();
             transform.Translate(Vector3.right * Time.deltaTime);
+        }
+
+        /// <summary> 巡邏怪行為 </summary>
+        protected void guardBehaviour()
+        {
+            if (stat == Stat.guard)
+            {
+                if (Vector3.Distance(GameManager.maze.GetChild(guardPoint[nextGrardNum]).position * Vector2.one, transform.position * Vector2.one) < 0.5f)
+                {
+                    print("arrive");
+                    if (++nextGrardNum >= 4)
+                    {
+                        nextGrardNum = 0;
+                    }
+                }
+                guard(guardPos);
+
+                if (Vector3.Distance(transform.position * Vector2.one, MinDisPlayer().position * Vector2.one) < 3)
+                {
+                    stat = Stat.pursue;
+                }
+            }
+            else if (stat == Stat.pursue)
+            {
+                target = MinDisPlayer();
+                if (!pursuePos.Contains((int)transform.position.x * MapCreater.totalCol[MapCreater.level] + (int)transform.position.y))
+                {
+                    stat = Stat.back;
+                }
+            }
+            else if (stat == Stat.back)
+            {
+                guard(pursuePos);
+                HP = MaxHP;
+                if (guardPos.Contains((int)transform.position.x * MapCreater.totalCol[MapCreater.level] + (int)transform.position.y))
+                {
+                    stat = Stat.guard;
+                }
+            }
+            moveToTarget();
+        }
+        protected void guard(HashSet<int> canGo)
+        {
+            Vector3 endPos = GameManager.maze.GetChild(guardPoint[nextGrardNum]).position;
+            Debug.Log(endPos);
+            int[] endRow = new int[1] { (int)endPos.x }, endCol = new int[1] { (int)endPos.y };
+            setNavigateTarget(endRow, endCol, canGo);
+        }
+
+
+        protected void Attack()
+        {
+            if ((CDTimer += Time.deltaTime) >= CD)
+            {
+                if (Vector3.Distance(transform.position * Vector2.one, MinDisPlayer().position * Vector2.one) < hand)
+                {
+                    if ((PreparationTimer += Time.deltaTime) >= Preparation)
+                    {
+                        Instantiate(attack, transform.position, transform.rotation);
+                        CDTimer = 0;
+                        PreparationTimer = 0;
+                    }
+                }
+                else
+                {
+                    PreparationTimer = 0;
+                }
+            }
         }
     }
 }
--- Assets/Script/Tauren.cs
+++ Assets/Script/Tauren.cs
@@ -6,16 +6,6 @@ namespace com.DungeonPad
 {
     public class Tauren : MonsterManager
     {
-        public int[] guardPoint;
-        public int nextGrardNum;
-        enum Stat
-        {
-            guard,
-            pursue,
-            back
-        }
-        Stat stat = Stat.guard;
-
         void Start()
         {
             int i, j, t =0;
@@ -42,49 +32,8 @@ namespace com.DungeonPad
 
         void Update()
         {
-            if(stat == Stat.guard)
-            {
-                if (Vector3.Distance(GameManager.maze.GetChild(guardPoint[nextGrardNum]).position * Vector2.one, transform.position * Vector2.one) < 0.5f)
-                {
-                    print("arrive");
-                    if (++nextGrardNum >= 4)
-                    {
-                        nextGrardNum = 0;
-                    }
-                }
-                guard(guardPos);
-
-                if (Vector3.Distance(transform.position*Vector2.one, MinDisPlayer().position * Vector2.one) < 3)
-                {
-                    stat = Stat.pursue;
-                }
-            }
-            else if(stat == Stat.pursue)
-            {
-                target = MinDisPlayer();
-                if (!pursuePos.Contains((int)transform.position.x * MapCreater.totalCol[MapCreater.level] + (int)transform.position.y))
-                {
-                    stat = Stat.back;
-                }
-            }
-            else if(stat == Stat.back)
-            {
-                guard(pursuePos);
-                HP = MaxHP;
-                if (guardPos.Contains((int)transform.position.x * MapCreater.totalCol[MapCreater.level] + (int)transform.position.y))
-                {
-                    stat = Stat.guard;
-                }
-            }
-            moveToTarget();
-        }
-
-        void guard(HashSet<int> canGo)
-        {
-            Vector3 endPos = GameManager.maze.GetChild(guardPoint[nextGrardNum]).position;
-            Debug.Log(endPos);
-            int[] endRow = new int[1] { (int)endPos.x }, endCol = new int[1] { (int)endPos.y };
-            setNavigateTarget(endRow, endCol, canGo);
+            guardBehaviour();
+            Attack();
         }
     }
 }
