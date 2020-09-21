9d8b96d008814b46b5c7a7a60f169a4abce1c99c
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-09-21 23:33:53 +0800

    出口迴避完成

---
+++ Assets/Script/Exit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace com.DungeonPad
+{
+    public class Exit : MonoBehaviour
+    {
+        float time = 3, timer;
+        public Transform MinDisPlayer()
+        {
+            int i;
+            float minDis = float.MaxValue;
+            Transform minDisPlayer = null;
+            for (i = 0; i < GameManager.players.childCount; i++)
+            {
+                Transform player = GameManager.players.GetChild(i);
+                if (minDis > Vector3.Distance(player.position, transform.position))
+                {
+                    minDis = Vector3.Distance(player.position, transform.position);
+                    minDisPlayer = player;
+                }
+            }
+            return minDisPlayer;
+        }
+
+        void Update()
+        {
+            Vector3 minDisPlayerDir = (transform.position * Vector2.one - MinDisPlayer().position * Vector2.one);
+            transform.Translate(minDisPlayerDir.normalized * (Time.deltaTime * 4));
+            int i;
+            if (minDisPlayerDir.sqrMagnitude < 0.5f)
+            {
+                if ((timer += Time.deltaTime) >= time)
+                {
+                    SceneManager.LoadScene("Win");
+                }
+            }
+            else
+            {
+                timer = 0;
+            }
+        }
+    }
+}
