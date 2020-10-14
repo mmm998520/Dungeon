5c85385dd974decc2b394e39e01ccd4712dfcc5c
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2020-10-14 23:03:08 +0800

    製作出口旋鈕(失敗

---
+++ Assets/ExitToucher.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class ExitToucher : MonoBehaviour
+    {
+        public static int touchNum;
+        public string playerName;
+        public GameObject joy;
+        Rigidbody2D rigidbody2D;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (touchNum >= 2)
+            {
+                joy.AddComponent(typeof(Rigidbody2D));
+                rigidbody2D = joy.GetComponent<Rigidbody2D>();
+                rigidbody2D.gravityScale = 0;
+            }
+            else
+            {
+                Destroy(rigidbody2D);
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.name == playerName)
+            {
+                touchNum++;
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collider)
+        {
+            if (collider.name == playerName)
+            {
+                touchNum--;
+            }
+        }
+    }
+}
---
+++ Assets/Script/ExitRoller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class ExitRoller : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.localPosition = Vector3.zero;
+        }
+    }
+}
