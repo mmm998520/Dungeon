54f8f28b5d551c88cac66340ce9c4ddb64be1dfe
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-10-29 09:30:35 +0800

    設置方形關卡

---
+++ Assets/Script/MapCreat/TaurenRoomControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class TaurenRoomControler : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
