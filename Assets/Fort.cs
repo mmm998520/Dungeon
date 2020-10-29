0ee1e71f25904ab4a7a05211bee05454cf4a7262
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-10-29 11:55:54 +0800

    fort基本規則未完成

---
+++ Assets/Fort.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class Fort : MonoBehaviour
+    {
+        static int playerTargetNum = 0;
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+            lookAtPlayer();
+        }
+
+        void lookAtPlayer()
+        {
+            if (playerTargetNum++ > 2)
+            {
+                playerTargetNum = 0;
+            }
+            transform.LookAt(GameManager.players.GetChild(0));
+            transform.Rotate(new Vector3(0, -90, 0));
+        }
+    }
+}
