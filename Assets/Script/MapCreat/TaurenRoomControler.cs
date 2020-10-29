45848c3d386e12d7983f842d86076284e6e5e629
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2020-10-29 21:08:26 +0800

    小牛頭人生成完成

--- Assets/Script/MapCreat/TaurenRoomControler.cs
+++ Assets/Script/MapCreat/TaurenRoomControler.cs
@@ -7,7 +7,8 @@ namespace com.DungeonPad
     public class TaurenRoomControler : MonoBehaviour
     {
         public TaurenBoss taurenBoss;
-        public Transform centerWall;
+        public Transform centerWall, TaurenInsPoses;
+        public GameObject tauren;
         HashSet<Transform> usedFort = new HashSet<Transform>();
 
         void Start()
@@ -17,12 +18,14 @@ namespace com.DungeonPad
 
         void Update()
         {
+            //防止條動畫失誤陷入無限迴圈
             if(usedFort.Count> centerWall.childCount - 4)
             {
                 usedFort.Clear();
             }
         }
 
+        #region//Boss
         public void setTaurenBossCanWalk(int _canWalk)
         {
             taurenBoss.canWalk = (_canWalk > 0);
@@ -46,7 +49,9 @@ namespace com.DungeonPad
         {
             taurenBoss.Armor = taurenBoss.MaxArmor;
         }
+        #endregion
 
+        #region//砲台
         public void selectOneFort()
         {
             int r;
@@ -64,6 +69,43 @@ namespace com.DungeonPad
         public void clearUsedFort()
         {
             usedFort.Clear();
+        }
+        #endregion
+
+        public void setTaurenInsPosesRotate()
+        {
+            float angle = Vector3.SignedAngle(Vector3.right, CameraManager.center * Vector2.one, Vector3.forward);
+            if (angle > -135 && angle <= -45)
+            {
+                TaurenInsPoses.rotation = Quaternion.Euler(0, 0, 270);
+            }
+            else if (angle > -45 && angle <= 45)
+            {
+                TaurenInsPoses.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (angle > 45 && angle <= 135)
+            {
+                TaurenInsPoses.rotation = Quaternion.Euler(0, 0, 90);
+            }
+            else
+            {
+                TaurenInsPoses.rotation = Quaternion.Euler(0, 0, 180);
+            }
+        }
+
+        public void insTaurenByOrder(int Order)
+        {
+            if (Order != 4)
+            {
+                for(int i=0;i< TaurenInsPoses.GetChild(Order).childCount; i++)
+                {
+                    Instantiate(tauren, TaurenInsPoses.GetChild(Order).GetChild(i).position, Quaternion.identity);
+                }
+            }
+            else
+            {
+                Instantiate(tauren, TaurenInsPoses.GetChild(Order).GetChild(Random.Range(0, TaurenInsPoses.GetChild(Order).childCount)).position, Quaternion.identity);
+            }
         }
     }
 }
