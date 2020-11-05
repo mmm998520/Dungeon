8dbccf62467767f8f66fa07d3c4845082f4c25f8
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2020-11-05 14:16:13 +0800

    蜘蛛鬥氣添加

---
+++ Assets/Script/role/GrudgeRandomer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class GrudgeRandomer : MonoBehaviour
+    {
+        SpriteRenderer spriteRenderer;
+        public Sprite[] sprites;
+
+        void Start()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        void Update()
+        {
+            spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        }
+    }
+}
