2eef66b5294c0682ce0bb782b4ad0e837430fa82
Ayumiyumi <1999yumi1158@gmail.com>
2020-11-22 18:17:59 +0800

    UI控制

---
+++ Assets/Script/UI/loadscene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadscene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    
+    public void selestgame()
+    {
+      SceneManager.LoadScene("SelectRole_Game");
+    }
+
+    public void selecttutorial()
+    {
+        SceneManager.LoadScene("SelectRole_Tutorial");
+    }
+
+    public void selectSetting()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            Debug.Log("e");
+            SceneManager.LoadScene("Home");
+        }
+    }
+
+}
