f0817259e1f3b6ab3f87e13289516b1f414a4f8a
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2020-11-25 15:39:33 +0800

    調整深淵、牆壁圖、置入戰鬥狀態、混亂狀態ui

--- Assets/Script/SpriteRandomer.cs
+++ Assets/Script/SpriteRandomer.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class SpriteRandomer : MonoBehaviour
 {
     public Sprite[] sprites;
+    public bool wall;
     void Start()
     {
         GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
-        transform.rotation = Quaternion.Euler(0, 180 * Random.Range(0, 2), 0);
+        if (!wall)
+        {
+            transform.rotation = Quaternion.Euler(0, 180 * Random.Range(0, 2), 0);
+        }
+        else
+        {
+            int r = Random.Range(0, 2);
+            transform.parent.GetChild(0).rotation = Quaternion.Euler(0, 180 * r, 0);
+            transform.parent.GetChild(1).rotation = Quaternion.Euler(0, 180 * r, 0);
+        }
     }
 
     void Update()
