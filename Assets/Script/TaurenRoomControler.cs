27b6789c7ed4e5a86ee003e6c581334e8cdd9f72
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2020-12-04 12:11:55 +0800

    Boss房時間軸製作(未製作擲斧

---
+++ Assets/Script/TaurenRoomControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class TaurenRoomControler : MonoBehaviour
+    {
+        public Transform monsters;
+        public GameObject spider, spiderB, spiderC, Boss;
+        public void insSpider(int times)
+        {
+            ins(spider, times);
+        }
+
+        public void insSpiderB(int times)
+        {
+            ins(spiderB, times);
+        }
+
+        public void insSpiderC(int times)
+        {
+            ins(spiderC, times);
+        }
+
+        void ins(GameObject obj, int times)
+        {
+            for(int i = 0; i < times; i++)
+            {
+                Instantiate(obj, new Vector3(Random.Range(1f, 20f), Random.Range(1f, 10f), 0), Quaternion.identity, monsters).SetActive(true);
+            }
+        }
+
+        public void setBossActive(int active)
+        {
+            Boss.SetActive(active > 0);
+        }
+    }
+}
