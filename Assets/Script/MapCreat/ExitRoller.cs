50a01b6b8fbf012bb218f6edccc9e4768141fd97
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2020-12-13 14:54:07 +0800

    旋轉門完成

--- Assets/Script/MapCreat/ExitRoller.cs
+++ Assets/Script/MapCreat/ExitRoller.cs
@@ -7,35 +7,12 @@ namespace com.DungeonPad
 {
     public class ExitRoller : MonoBehaviour
     {
-        float rotationZ;
-        bool canUse = true;
-        float useSpeed = 30, notUseSpeed = 120;
-        public SpriteRenderer spriteRenderer;
-        public Sprite non, used;
-
         void Update()
         {
-            if(ExitToucher.P1touch && ExitToucher.P2touch && canUse)
-            {
-                spriteRenderer.sprite = used;
-                rotationZ += Time.deltaTime * useSpeed;
-            }
-            else
-            {
-                spriteRenderer.sprite = non;
-                rotationZ -= Time.deltaTime * notUseSpeed;
-                canUse = false;
-            }
-            rotationZ = Mathf.Clamp(rotationZ, 0, 370);
-            transform.rotation = Quaternion.Euler(0, 0, rotationZ);
-
-            if (rotationZ>360)
+            transform.localPosition = Vector3.zero;
+            if (transform.rotation.eulerAngles.z > 180 && transform.rotation.eulerAngles.z < 200)
             {
                 SceneManager.LoadScene("Game 2");
-            }
-            else if (rotationZ <= 0)
-            {
-                canUse = true;
             }
         }
     }
--- Assets/Script/MapCreat/ExitToucher.cs
+++ Assets/Script/MapCreat/ExitToucher.cs
@@ -6,46 +6,42 @@ namespace com.DungeonPad
 {
     public class ExitToucher : MonoBehaviour
     {
-        public static bool P1touch, P2touch;
-        bool haveTouch = false;
+        public static int touchNum;
         public string playerName;
-
+        public Rigidbody2D rigidbody2D;
         void Start()
         {
 
         }
 
+        // Update is called once per frame
         void Update()
         {
-            haveTouch = false;
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.66f);
-            for(int i = 0; i < colliders.Length; i++)
+            if (touchNum >= 2)
             {
-                if(colliders[i].name == playerName)
-                {
-                    if(playerName == "p1")
-                    {
-                        P1touch = true;
-                    }
-                    if (playerName == "p2")
-                    {
-                        P2touch = true;
-                    }
-                    haveTouch = true;
-                }
+                rigidbody2D.mass = 0.2f;
             }
-            if(!haveTouch)
+            else
             {
-                if (playerName == "p1")
-                {
-                    P1touch = false;
-                }
-                if (playerName == "p2")
-                {
-                    P2touch = false;
-                }
+                rigidbody2D.mass = 1000000;
+                rigidbody2D.velocity = Vector3.zero;
+                rigidbody2D.angularVelocity = 0;
             }
+        }
 
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.name == playerName)
+            {
+                touchNum++;
+            }
+        }
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.collider.name == playerName)
+            {
+                touchNum--;
+            }
         }
     }
 }
