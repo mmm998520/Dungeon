c3c49c2f054614acfddde916ab9a7874ce53ab66
Ayumiyumi <1999yumi1158@gmail.com>
2020-12-20 16:03:59 +0800

    角色殘影

---
+++ Assets/Script/InsAfterImages.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsAfterImages : MonoBehaviour
+{
+    public GameObject dash, afterImg;
+    public float timer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 0.1)
+        {
+            Instantiate(afterImg, new Vector2(dash.transform.position.x, dash.transform.position.y), dash.transform.rotation);
+            timer = 0;
+        }
+    }
+}
