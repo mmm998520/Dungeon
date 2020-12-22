0aa10fb75e7a3b589fcb65bbfcf4816a6300298b
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2020-12-22 15:42:13 +0800

    測試調整、HP預警邊框

---
+++ Assets/PlayerHPVolumeSet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace com.DungeonPad
+{
+    public class PlayerHPVolumeSet : MonoBehaviour
+    {
+        Vignette vignette;
+        float speed = 5;
+        bool lerpToMax = true;
+        float lerpMax = 0.4f, lerpMin = 0.15f;
+        void Start()
+        {
+            gameObject.GetComponent<Volume>().profile.TryGet(out vignette);
+        }
+
+        void Update()
+        {
+            print(PlayerManager.HP);
+            if (PlayerManager.HP < 30)
+            {
+                if (lerpToMax)
+                {
+                    vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, lerpMax, Time.deltaTime * speed);
+                    if(lerpMax - vignette.intensity.value < 0.01f)
+                    {
+                        lerpToMax = false;
+                    }
+                }
+                else
+                {
+                    vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, lerpMin, Time.deltaTime * speed);
+                    if (vignette.intensity.value - lerpMin < 0.01f)
+                    {
+                        lerpToMax = true;
+                    }
+                }
+                print(vignette.intensity.value);
+            }
+            else
+            {
+                vignette.intensity.value = Mathf.Lerp(vignette.intensity.value, 0f, Time.deltaTime * speed);
+            }
+        }
+    }
+}
