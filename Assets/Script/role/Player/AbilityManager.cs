764287fc4a3e03fc67e2c0188b508f128b5f430e
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-12-31 12:02:41 +0800

    能力商店雛型

--- Assets/Script/role/Player/AbilityManager.cs
+++ Assets/Script/role/Player/AbilityManager.cs
@@ -6,14 +6,31 @@ namespace com.DungeonPad
 {
     public class AbilityManager : MonoBehaviour
     {
-        void Start()
+        public static List<string> allAbility = new List<string>();
+        public static List<string> myAbilitys = new List<string>();
+        void Awake()
         {
-
+            setAllAbility();
         }
 
         void Update()
         {
 
+        }
+
+        void setAllAbility()
+        {
+            allAbility.Clear();
+            allAbility.Add("光儲存上限增(40→60)");
+            allAbility.Add("光儲存上限增(60→80)");
+            allAbility.Add("殺怪回血10");
+            allAbility.Add("殺怪回血25");
+            allAbility.Add("原地復活光球+1");
+            allAbility.Add("原地復活上限+1(同時送1顆)");
+            allAbility.Add("衝刺距離增加 4變5.5");
+            allAbility.Add("衝刺冷卻時間降低0.1");
+            allAbility.Add("一般移動速度加快");
+            allAbility.Add("瞬移回夥伴身邊(冷卻10秒)");
         }
     }
 }
--- Assets/Script/role/Player/AbilityStore.cs
+++ Assets/Script/role/Player/AbilityStore.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace com.DungeonPad
 {
     public class AbilityStore : MonoBehaviour
     {
+        List<string> storeAbility = new List<string>();
+        List<string> canBuys;
+        List<int> storeAbilityPrice = new List<int>();
+        public Text[] storeAbilityText;
+        public Text[] storeAbilityPriceText;
+        public GameObject panel;
+        int storeCanbuyNum = 3;
         void Start()
         {
             
@@ -14,6 +22,119 @@ namespace com.DungeonPad
         void Update()
         {
 
+        }
+
+        void showStore()
+        {
+            panel.SetActive(true);
+            setCanBuyAbilitys();
+            randomAbility();
+            setPrice();
+            showOnStore();
+        }
+
+        void setCanBuyAbilitys()
+        {
+            canBuys = AbilityManager.allAbility;
+            for(int i = 0; i < AbilityManager.myAbilitys.Count; i++)
+            {
+                if (canBuys.Contains(AbilityManager.myAbilitys[i]))
+                {
+                    canBuys.Remove(AbilityManager.myAbilitys[i]);
+                }
+            }
+            canBuys = canBuyRemoveCantHave(canBuys, "光儲存上限增(40→60)", "光儲存上限增(60→80)");
+        }
+
+        List<string> canBuyRemoveCantHave(List<string> canBuys, string first, string next)
+        {
+            if (!AbilityManager.myAbilitys.Contains(first))
+            {
+                canBuys.Remove(next);
+            }
+            return canBuys;
+        }
+
+        void randomAbility()
+        {
+            storeAbility.Clear();
+            for(int i = 0; i < storeCanbuyNum; i++)
+            {
+                int r = Random.Range(0, canBuys.Count);
+                storeAbility.Add(canBuys[r]);
+                canBuys.RemoveAt(r);
+                if (canBuys.Count <= 0)
+                {
+                    Debug.LogError("沒東西可以賣了");
+                    break;
+                }
+            }
+        }
+
+        void setPrice()
+        {
+            storeAbilityPrice.Clear();
+            for (int i = 0; i < storeCanbuyNum; i++)
+            {
+                if (i < storeAbility.Count)
+                {
+                    switch (storeAbility[i])
+                    {
+                        case "光儲存上限增(40→60)":
+                            storeAbilityPrice.Add(Random.Range(20, 21));
+                            break;
+                        case "光儲存上限增(60→80)":
+                            storeAbilityPrice.Add(Random.Range(25, 26));
+                            break;
+                        case "殺怪回血10":
+                            storeAbilityPrice.Add(Random.Range(20, 21));
+                            break;
+                        case "殺怪回血25":
+                            storeAbilityPrice.Add(Random.Range(25, 26));
+                            break;
+                        case "原地復活光球+1":
+                            storeAbilityPrice.Add(Random.Range(20, 21));
+                            break;
+                        case "原地復活上限+1(同時送1顆)":
+                            storeAbilityPrice.Add(Random.Range(35, 36));
+                            break;
+                        case "衝刺距離增加 4變5.5":
+                            storeAbilityPrice.Add(Random.Range(30, 41));
+                            break;
+                        case "衝刺冷卻時間降低0.1":
+                            storeAbilityPrice.Add(Random.Range(30, 41));
+                            break;
+                        case "一般移動速度加快":
+                            storeAbilityPrice.Add(Random.Range(30, 41));
+                            break;
+                        case "瞬移回夥伴身邊(冷卻10秒)":
+                            storeAbilityPrice.Add(Random.Range(35, 46));
+                            break;
+                    }
+                }
+                else
+                {
+                    storeAbilityPrice.Add(Random.Range(0, 1));
+                }
+            }
+        }
+
+        void showOnStore()
+        {
+            storeAbilityPrice.Clear();
+            for (int i = 0; i < storeCanbuyNum; i++)
+            {
+                if(i< storeAbility.Count)
+                {
+                    storeAbilityText[i].text = storeAbility[i];
+                    storeAbilityPriceText[i].text = "" + storeAbilityPrice[i];
+                }
+                else
+                {
+                    storeAbilityText[i].text = "null";
+                    storeAbilityPriceText[i].text = "null";
+                }
+            }
         }
     }
 }
