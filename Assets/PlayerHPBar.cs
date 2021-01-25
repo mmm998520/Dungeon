759d38460d9ff57ca9e8adb0dfd21efb6b663e48
Simon <mmm998520@gmail.com>
2021-01-25 12:04:01 +0800

    血條完成(未實裝至其他Scene)

---
+++ Assets/PlayerHPBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.DungeonPad
+{
+    public class PlayerHPBar : MonoBehaviour
+    {
+        public RectTransform mask, color;
+        public Image HPBar_Color;
+        public Sprite HPBar_Red, HPBar_Yellow, HPBar_Green;
+        float hpRate, hpRateRecorder;
+        void Update()
+        {
+            hpRate = PlayerManager.HP / PlayerManager.MaxHP;
+            mask.localScale = new Vector3(hpRate, 1, 1);
+            color.localScale = new Vector3(1/hpRate, 1, 1);
+            if (hpRate > 0.9999f)
+            {
+                HPBar_Color.sprite = HPBar_Green;
+            }
+            else if(hpRate < hpRateRecorder - 0.001f)
+            {
+                HPBar_Color.sprite = HPBar_Red;
+            }
+            else if (hpRate > hpRateRecorder + 0.001f)
+            {
+                HPBar_Color.sprite = HPBar_Green;
+            }
+            else
+            {
+                HPBar_Color.sprite = HPBar_Yellow;
+            }
+            hpRateRecorder = hpRate;
+        }
+    }
+}
