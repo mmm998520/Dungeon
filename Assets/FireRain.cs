ce45104a153556be715c3d148137b93f5cea990c
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2021-01-26 23:10:17 +0800

    火雨(未完成)

---
+++ Assets/FireRain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class FireRain : MonoBehaviour
+    {
+        public float timer, stopeTimer, destoryTimer;
+        public bool CanHit = false;
+        public SpriteRenderer spriteRenderer;
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer >= stopeTimer + destoryTimer)
+            {
+                Destroy(gameObject);
+            }
+            else if(timer >= stopeTimer)
+            {
+                CanHit = true;
+                spriteRenderer.color = Color.white;
+            }
+        }
+    }
+}
---
+++ Assets/FireRains.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class FireRains : MonoBehaviour
+    {
+        public Vector2[] playerPos;
+        public HashSet<PlayerManager> hited;
+        public float rainAttackDistance;
+
+        void Start()
+        {
+            playerPos = new Vector2[GameManager.players.childCount];
+        }
+
+        void LateUpdate()
+        {
+            hited = new HashSet<PlayerManager>();
+            int i, j;
+            Transform child;
+
+            for (i = 0; i < playerPos.Length; i++)
+            {
+                playerPos[i] = GameManager.players.GetChild(i).position;
+            }
+
+            for(i = 0; i < playerPos.Length; i++)
+            {
+                for (j=0;j< transform.childCount; j++)
+                {
+                    child = transform.GetChild(j);
+                    if (Vector3.Distance(child.position, playerPos[i]) < rainAttackDistance && child.GetComponent<FireRain>().CanHit)
+                    {
+                        hited.Add(GameManager.players.GetChild(i).GetComponent<PlayerManager>());
+                        break;
+                    }
+                }
+            }
+
+            for (i = 0; i < hited.Count; i++)
+            {
+                PlayerManager.HP -= 1 * Time.deltaTime;
+            }
+        }
+    }
+}
