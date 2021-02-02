f2cdbbb9a755572e29bcb59c7050ed19d530089c
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2021-02-02 20:48:58 +0800

    新手教學完成

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -47,6 +47,14 @@ namespace com.DungeonPad
         {
             //if (abilityStore)
             {
+                if (Input.GetKeyDown(KeyCode.F1))
+                {
+                    PlayerManager.Life += 1;
+                }
+                if (Input.GetKeyDown(KeyCode.F2))
+                {
+                    PlayerManager.money -= 5;
+                }
                 if ((emptyRoomNumCountTimer += Time.deltaTime) >= 1)
                 {
                     emptyRoomNumCountTimer = 0;
---
+++ Assets/Tutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.DungeonPad
+{
+    public class Tutorial : MonoBehaviour
+    {
+        Color textColor;
+        public Text[] texts;
+        public int[] ShowTextCenters;
+        public int ShowHPLine;
+        public Image[] images;
+        // Start is called before the first frame update
+        void Start()
+        {
+            textColor = texts[0].color;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            for(int i = 0; i < texts.Length; i++)
+            {
+                texts[i].color = new Color(textColor.r, textColor.g, textColor.b, 1 - Mathf.Abs(CameraManager.center.x - ShowTextCenters[i]) * 0.2f);
+            }
+            for(int i = 0; i < images.Length; i++)
+            {
+                images[i].color = new Color(images[i].color.r, images[i].color.g, images[i].color.b, (ShowHPLine - CameraManager.center.x) / ShowHPLine);
+            }
+        }
+    }
+}
