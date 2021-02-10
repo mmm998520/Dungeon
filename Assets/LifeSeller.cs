48fe40f3dc8cc7af5793c528d328263f7c627b12
Simon <mmm998520@gmail.com>
2021-02-11 00:14:33 +0800

    水晶商人圖示完成

--- Assets/LifeSeller.cs
+++ Assets/LifeSeller.cs
@@ -8,6 +8,9 @@ namespace com.DungeonPad
     {
         public int price;
         public int times;
+
+        public List<Transform> brightOrder;
+        Color gray = new Color(0.3f, 0.3f, 0.3f, 1);
         private void Update()
         {
 
@@ -16,12 +19,23 @@ namespace com.DungeonPad
         {
             if (PlayerManager.money >= 5 && PlayerManager.Life < PlayerManager.MaxLife)
             {
+                for(int i = 0; i < brightOrder[times].childCount; i++)
+                {
+                    brightOrder[times].GetChild(i).GetComponent<SpriteRenderer>().color = Color.white;
+                }
                 times++;
                 PlayerManager.money -= 5;
                 if (times >= price / 5)
                 {
                     times = 0;
                     PlayerManager.Life += 1;
+                    for(int i = 0; i < brightOrder.Count; i++)
+                    {
+                        for(int j = 0; j < brightOrder[i].childCount; j++)
+                        {
+                            brightOrder[i].GetChild(j).GetComponent<SpriteRenderer>().color = gray;
+                        }
+                    }
                 }
             }
         }
