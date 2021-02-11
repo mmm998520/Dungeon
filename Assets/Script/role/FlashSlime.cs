e9f9b7ed81abd188257ece6082c3bc4c9a82680b
Simon <mmm998520@gmail.com>
2021-02-11 22:08:58 +0800

    閃光史萊姆完成

---
+++ Assets/Script/role/FlashSlime.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace com.DungeonPad
+{
+    public class FlashSlime : MonsterManager
+    {
+        public enum SlimeBehavior
+        {
+            ramdomMove = 0,
+            stop = 1
+        }
+        public SlimeBehavior slimeBehavior;
+
+        bool stoping = false;
+        public Animator SpriteAnimator;
+        public float flashTimer;
+        public Light2D light2D;
+
+        void Start()
+        {
+            ReCD();
+            startRoomRow = Mathf.RoundToInt(transform.position.x) / GameManager.mazeCreater.objectCountRowNum;
+            startRoomCol = Mathf.RoundToInt(transform.position.y) / GameManager.mazeCreater.objectCountColNum;
+            arriveNewRoom(startRoomRow, startRoomCol);
+            randomTarget();
+        }
+
+        void Update()
+        {
+            if (slimeBehavior == SlimeBehavior.ramdomMove)
+            {
+                if ((CDTimer += Time.deltaTime) >= CD)
+                {
+                    if (!stoping)
+                    {
+                        stoping = true;
+                        GetComponent<Animator>().SetTrigger("Stop");
+                    }
+                    rigidbody.velocity = Vector3.zero;
+                    rigidbody.angularDrag = 0;
+                    SpriteAnimator.SetBool("Stop", true);
+                }
+                else
+                {
+                    Move();
+                    Stuck(1, 1);
+                    stoping = false;
+                    SpriteAnimator.SetBool("Stop", false);
+                }
+            }
+
+            if (Vector2.Distance(transform.position, MinDisPlayer().position) < 6)
+            {
+                flashTimer += Time.deltaTime;
+                if (flashTimer >= 5)
+                {
+                    boom();
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                flashTimer = Mathf.Lerp(flashTimer, 0, Time.deltaTime * 5);
+            }
+            light2D.intensity = Mathf.Sin(Mathf.Pow(flashTimer, 3)) * 5;
+        }
+
+        void boom()
+        {
+            ShowFlashSlimeLight.LightTimer = 5;
+        }
+        /*
+        bool Blew = false;
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.GetComponent<PlayerManager>())
+            {
+                collision.collider.GetComponent<PlayerManager>().StickTimer = 0;
+                attackSource.Play();
+                attackSource.transform.parent = null;
+                Destroy(gameObject);
+                Blew = true;
+                Destroy(attackSource.gameObject, 5);
+            }
+        }
+        */
+        public override void beforeDied()
+        {
+            base.beforeDied();
+            GameManager.KillSpider++;
+            SpriteAnimator.transform.parent = null;
+            SpriteAnimator.SetTrigger("Destroy");
+        }
+    }
+}
---
+++ Assets/ShowFlashSlimeLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowFlashSlimeLight : MonoBehaviour
+{
+    public Image image;
+    public static float LightTimer = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (LightTimer > 0)
+        {
+            LightTimer -= Time.deltaTime;
+            image.color = new Color(1, 1, 1, Mathf.Atan(LightTimer *4/5 - 2f));
+        }
+        else
+        {
+            image.color = Color.clear;
+        }
+    }
+}
