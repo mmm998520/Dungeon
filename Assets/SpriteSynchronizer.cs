e3d28a0e9320c79ecb6b984abce42ef0e7d86c90
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2021-02-28 11:33:44 +0800

    龍關亮度自適應調整

--- Assets/SpriteSynchronizer.cs
+++ Assets/SpriteSynchronizer.cs
@@ -8,13 +8,25 @@ namespace com.DungeonPad
     {
         [SerializeField] SpriteRenderer targetRenderer;
         SpriteRenderer myRenderner;
+        [SerializeField]bool alphaChanger;
+        float targetRendererA, myRendernerA;
         void Start()
         {
             myRenderner = GetComponent<SpriteRenderer>();
+            targetRendererA = targetRenderer.color.a;
+            myRendernerA = myRenderner.color.a;
+            if (alphaChanger)
+            {
+                myRenderner.color = new Color(myRenderner.color.r, myRenderner.color.g, myRenderner.color.b, 0);
+            }
         }
         void Update()
         {
             myRenderner.sprite = targetRenderer.sprite;
+            if (alphaChanger)
+            {
+                myRenderner.color = new Color(myRenderner.color.r, myRenderner.color.g, myRenderner.color.b, myRendernerA * targetRenderer.color.a / targetRendererA);
+            }
         }
     }
 }
