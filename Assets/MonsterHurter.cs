7c9b6eb36cec644807c1529c40b9bf98bb84ea91
DESKTOP-84BO59T\user <mmm998520@gmail.com>
2021-03-14 14:49:09 +0800

    打擊感動畫重製(未完成

---
+++ Assets/MonsterHurter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.DungeonPad
+{
+    public class MonsterHurter : MonoBehaviour
+    {
+        [SerializeField] SpriteRenderer mySpriteRenderer, targetSpriteRenderer;
+        public float timer;
+        void Start()
+        {
+            if (!mySpriteRenderer)
+            {
+                targetSpriteRenderer = GetComponent<SpriteRenderer>();
+            }
+            if (!targetSpriteRenderer)
+            {
+                try
+                {
+                    targetSpriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+                }
+                catch
+                {
+                    Debug.LogError("沒有target", gameObject);
+                }
+            }
+        }
+
+        void Update()
+        {
+            if (timer < 1)
+            {
+                try
+                {
+                    mySpriteRenderer.sprite = Resources.Load<Sprite>("Pictures/MonsterHurt/" + targetSpriteRenderer.sprite.name);
+                }
+                catch
+                {
+                    Debug.LogError("沒有這張圖");
+                }
+            }
+        }
+    }
+}
